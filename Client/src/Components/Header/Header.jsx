4222157b39509511c292d4fb401b54a39e3b1ed2
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-03-25 22:59:39 -0600

    Add Header component for the to-do list interface

---
+++ Client/src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='col-12 text-center bg-primary text-white'>
+        <i className='bi bi-check2-square me-1'></i>
+        TO-DO LIST
+    </div>
+  )
+}
+
+export default Header
