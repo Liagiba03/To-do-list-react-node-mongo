791a7456ad9c44ad4d994df9e28f6fe2a741d34a
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-03-25 22:59:08 -0600

    Add main entry point for React application with Bootstrap integration

---
+++ Client/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import Barra from './Components/Barra.jsx'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+import "bootstrap-icons/font/bootstrap-icons.min.css"
+import "./index.css"
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
